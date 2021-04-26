cd18e4c71b1e2270d51e57cc44a2cca7315bbe13
amira-rhouma <81182333+amira-rhouma@users.noreply.github.com>
2021-04-26 13:15:42 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+
+<div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+    
+  </div>
+    
+  );
+}
+
+export default App;
